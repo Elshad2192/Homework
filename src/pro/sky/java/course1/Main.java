102e1c5dfca54e036ad7dc1e7fa4f36fcf9ef4a6
Elshad <102314315+Elshad2192@users.noreply.github.com>
2022-03-29 10:11:34 +0500

    myFirstCommit

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,6 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
     }
 }
